dd6a68a01fa1431ea86e6c24022c511558f68558
LucasCoppola <lucascoppola21@gmail.com>
2023-12-26 12:34:57 -0300

    connect web socket

--- index-ws.js
+++ index-ws.js
@@ -8,3 +8,28 @@ app.get('/', (req, res) => {
 
 server.on('request', app)
 server.listen(3000, () => console.log('Server running on port 3000'))
+
+/** BEGIN WEBSOCKET */
+const WebSocketServer = require('ws').Server
+
+const wss = new WebSocketServer({ server: server })
+
+wss.on('connection', function connection(ws) {
+	const numClients = wss.clients.size
+	console.log('Clients connected:', numClients)
+
+	wss.broadcast(`Current Visitors: ${numClients}`)
+
+	if (ws.readyState === ws.OPEN) {
+		ws.send('Welcome to my server')
+	}
+
+	ws.on('close', function close() {
+		console.log('A client has desconnected')
+		wss.broadcast(`Current Visitors: ${numClients}`)
+	})
+})
+
+wss.broadcast = function broadcast(data) {
+	wss.clients.forEach((client) => client.send(data))
+}
